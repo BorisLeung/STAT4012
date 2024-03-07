ef65b9edfbb0f453d76dc06b8e19cfa1af121f1c
Boris Leung <borisleung2013@gmail.com>
2024-03-07 16:16:15 +0800

    Add preprocessing notebook

--- const.py
+++ const.py
@@ -1,3 +1,11 @@
+"""
+A python file that holds all constant values and global variables used in the project.
+"""
+
+import os
+
+DATA_DIR = "data"
+POSTER_DIR = os.path.join(DATA_DIR, "posters")
 GENRE = "genre"
 ID = "id"
 MOVIE_ID = "movie_id"
